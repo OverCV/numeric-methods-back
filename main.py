f10d4f0ec028901ccfff808071c9ce66c7ed8fd3
vogel <over.castrillon22396@ucaldas.edu.co>
2023-10-30 01:22:06 -0500

    Basic FastAPI workflow structure.

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+import uvicorn
+
+app: FastAPI = FastAPI()
+
+app.title = 'Numeric methods | Models'
+app.version = '1.0.0'
+
+origins = [
+    'http://localhost:4200',
+]
+
+# Routing
+
+app.add_middleware(
+    CORSMiddleware, allow_origins=origins,
+    allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
+)
+
+
+@app.get('/')
+async def root():
+    return {'message': 'Hello math!'}
+
+if __name__ == '__main__':
+    kwargs = {'host': 'localhost', 'port': 8000}
+    kwargs.update({'debug': True, 'reload': True})
+    uvicorn.run('main:app', **kwargs, reload=True)
