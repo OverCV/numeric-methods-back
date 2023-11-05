95c13959a5b1a7f0b1845448c59e67c1dd15f4c7
vogel <over.castrillon22396@ucaldas.edu.co>
2023-11-05 18:28:31 -0500

    Routing for approximation.

--- api/constants/const.py
+++ api/constants/const.py
@@ -1,2 +1,3 @@
 EXP_FUNCTION: str = 'x * E**(t)'
 DATA: str = 'data'
+NG_LOCALE_URL: str = 'http://localhost:4200'
--- api/main.py
+++ api/main.py
@@ -1,7 +1,9 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from middlewares.error import ErrorHandler
 
+from constants.const import NG_LOCALE_URL
 from database.db import Base, engine
 from routers import approx
 
@@ -12,17 +14,16 @@ app: FastAPI = FastAPI()
 app.title = 'Numeric methods | Models'
 app.version = '1.0.0'
 
-origins = [
-    'http://localhost:4200',
-]
-
-app.include_router(approx.router, tags=['Approx'], prefix='/approx')
-
+# Adicion de middlewares
+app.add_middleware(ErrorHandler)
 
 app.add_middleware(
-    CORSMiddleware, allow_origins=origins,
-    allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
+    CORSMiddleware,
+    allow_origins=[NG_LOCALE_URL], allow_credentials=True,
+    allow_methods=['*'], allow_headers=['*']
 )
+
+app.include_router(approx.router, tags=['Approxs'], prefix='/approx')
 
 
 @app.get('/')
